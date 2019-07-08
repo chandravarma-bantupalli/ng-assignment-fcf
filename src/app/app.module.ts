58be77745f99ed99c38c23ccdf0780397aecb531
chandravarma-bantupalli <chandravarmabantupalli@gmail.com>
2019-07-08 16:02:40 +0530

    added all the components in the home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { GamePlayModule } from './game-play/game-play.module';
@@ -13,6 +14,7 @@ import { GamePlayModule } from './game-play/game-play.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HomeModule,
     GamePlayModule
   ],
