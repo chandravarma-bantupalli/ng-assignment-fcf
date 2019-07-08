9b556d0e9b8ac3f0c2efe92835b5ce2fbcc56b60
chandravarma-bantupalli <chandravarmabantupalli@gmail.com>
2019-07-08 21:22:16 +0530

    added components in game-play module

---
+++ src/app/game-play/play-tile/play-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-tile',
+  templateUrl: './play-tile.component.html',
+  styleUrls: ['./play-tile.component.css']
+})
+export class PlayTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/game-play/player-score/player-score.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-score',
+  templateUrl: './player-score.component.html',
+  styleUrls: ['./player-score.component.css']
+})
+export class PlayerScoreComponent implements OnInit {
+
+  public playerName: string;
+  public score: number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
