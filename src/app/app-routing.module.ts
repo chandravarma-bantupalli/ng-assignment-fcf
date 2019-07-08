90fc792f6093fb179460475cb3d6d693688b34a8
chandravarma-bantupalli <chandravarmabantupalli@gmail.com>
2019-07-08 13:01:18 +0530

    added routes in the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { GameComponent } from './game-play/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: '**', pathMatch: 'prefix', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
